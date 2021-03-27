ba4d45127049309a888a847c9530c5927837f6a6
michaela stone <vlreasilon@gmail.com>
2021-03-27 13:19:08 +0300

    Js update to real data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 function displayTemp(response){
     let tempElement = document.querySelector("#currentTemp");
-    tempElement.innerHTML = math.round(response.deta.main.temp);
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#Humidity");
+    tempElement.innerHTML = math.round 
+    (response.deta.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.deta.weather[0].description;
+    humidityElement.innerHTML = response.deta.main.humidity;
+
 
 }
 
 
-let apiKey = "b9ce1431880d9b98b3e9bb639a33c0b3"
+let apiKey = "80c705941c8c0d5f55e120669c8a73f5"
 let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Tel Aviv&appid= ${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
