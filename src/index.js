3d91ef25bafa529b6da7f7068cc7403a8f7faff2
michaela stone <vlreasilon@gmail.com>
2021-03-28 21:14:32 +0300

    wind and humidity update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-function displayTemp(response){
+function displayTemp(response) {
     let tempElement = document.querySelector("#currentTemp");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#Humidity");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
     tempElement.innerHTML = math.round 
     (response.deta.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.deta.weather[0].description;
     humidityElement.innerHTML = response.deta.main.humidity;
+    windElement.innerHTML = math.round(response.deta.wind.speed); 
 
 
 }
 
 
 let apiKey = "80c705941c8c0d5f55e120669c8a73f5"
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=Tel Aviv&appid= ${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;;
 
 axios.get(apiUrl).then(displayTemp);
