315a9a5e5a56605d33db0ec78bb1f5447c7b56d0
michaela stone <vlreasilon@gmail.com>
2021-03-28 21:32:55 +0300

    time update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+ if (hours < 10) {
+    hours = `0${hours}`;
+let minutes = date.getMinutes();
+ if (minutes < 10) {
+    minutes = `0${minutes}`;
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
 
 
 
@@ -10,12 +29,14 @@ function displayTemp(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
     tempElement.innerHTML = math.round 
     (response.deta.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.deta.weather[0].description;
     humidityElement.innerHTML = response.deta.main.humidity;
     windElement.innerHTML = math.round(response.deta.wind.speed); 
+    dateElement.innerHTML = formatDate(response.data.dt*1000); 
 
 
 }
