93d7ad80230479108d4da7d50a502ef4c17cec17
michaela stone <vlreasilon@gmail.com>
2021-04-12 22:45:06 +0300

    Update index.js
    api addition

--- src/index.js
+++ src/index.js
@@ -53,6 +53,12 @@ forecastElement.innerHTML = forecastHTML;
 console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "80c705941c8c0d5f55e120669c8a73f5";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat= ${coordinates.lat}&lon= ${coordinates.lon}&appid= ${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemp(response) {
     let tempElement = document.querySelector("#temp");
@@ -72,6 +78,9 @@ function displayTemp(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000); 
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description);
+
+    getForecast(response.deta.coord);
+
 
 
 }
